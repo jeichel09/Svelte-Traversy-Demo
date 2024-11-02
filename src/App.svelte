a8609d647f78a14e3ee79062cc48bfbbe16fe8be
Daniel <dunyto@gmail.com>
2024-11-03 00:18:23 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	import FeedbackList from "./components/FeedbackList.svelte";
+
+	let feedback = [
+		{
+			id: 1,
+			rating: 10,
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.'
+		},
+		{
+			id: 2,
+			rating: 9,
+			text: 'Laboris quis incididunt enim cupidatat exercitation culpa magna aliquip cillum deserunt.'
+		},
+		{
+			id: 3,
+			rating: 7,
+			text: 'Ut quis exercitation consectetur consequat. Exercitation esse aute duis sit ex ipsum fugiat est.'
+		},
+	];
+
+	const deleteFeedback = (e) => {
+		const itemId = e.detail;
+		feedback = feedback.filter((item) => item.id !== itemId);	}
+</script>
+
+<main class="container">
+	<FeedbackList {feedback} on:delete-feedback={deleteFeedback}/>
+</main>
+
